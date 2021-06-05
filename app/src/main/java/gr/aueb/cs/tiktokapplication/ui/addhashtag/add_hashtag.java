197be837961d4927d30bbfa0ac1eac7a99951227
Dimitris Mamakas <dimitrisaxo55@gmail.com>
2021-06-05 11:38:19 +0300

    Added camera and videogallery functionality

--- app/src/main/java/gr/aueb/cs/tiktokapplication/ui/addhashtag/add_hashtag.java
+++ app/src/main/java/gr/aueb/cs/tiktokapplication/ui/addhashtag/add_hashtag.java
@@ -1,17 +1,15 @@
 package gr.aueb.cs.tiktokapplication.ui.addhashtag;
 
 import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
+import java.util.ArrayList;
 import gr.aueb.cs.tiktokapplication.R;
 
 public class add_hashtag extends Fragment {
@@ -23,16 +21,19 @@ public class add_hashtag extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_hashtag, container, false);
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AddHashtagViewModel.class);
-        // TODO: Use the ViewModel
+        ArrayList<String> aa = getActivity().getIntent().getExtras().getStringArrayList("INFO");
+
+        for (String e: aa) {
+            System.out.println(e);
+        }
+
+        View root = inflater.inflate(R.layout.fragment_add_hashtag, container, false);
+        TextView t = root.findViewById(R.id.channel_name_text_view);
+        t.setText("MALAKA");
+
+        return inflater.inflate(R.layout.fragment_add_hashtag, container, false);
     }
 
 }
